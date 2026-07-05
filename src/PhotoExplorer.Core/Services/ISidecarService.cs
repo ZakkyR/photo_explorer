6c1adc38ae1a2e6d1b6d4738da2715fb6a5b3d4d
Ryosuke Miyazaki <notzibli@outlook.com>
2026-07-05 19:57:54 +0900

    feat: ExportToSidecarAsync / ForceImportFromSidecarAsync を追加

--- src/PhotoExplorer.Core/Services/ISidecarService.cs
+++ src/PhotoExplorer.Core/Services/ISidecarService.cs
@@ -3,6 +3,8 @@ namespace PhotoExplorer.Core.Services;
 public interface ISidecarService : IDisposable
 {
     Task MergeIntoDbAsync(string folderPath);
+    Task ExportToSidecarAsync(string folderPath);
+    Task ForceImportFromSidecarAsync(string folderPath);
     Task AddEntryAsync(string filePath, string tagName);
     Task AddEntryBulkAsync(IReadOnlyList<string> filePaths, string tagName);
     Task RemoveEntryAsync(string filePath, string tagName);
